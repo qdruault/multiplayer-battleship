cf0b90727e0570b639612ec9d80f18780a3743b1
tboulair <tboulair@utc>
2017-10-17 18:25:08 +0200

    Nouveau package data module.

---
+++ Battleship/src/main/java/com/utclo23/data/module/GameMediator.java
@@ -0,0 +1,15 @@
+package com.utclo23.data.module;
+
+import com.utclo23.data.structure.Game;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author tboulair
+ */
+public class GameMediator {
+    private ArrayList<Game> gamesList;
+    
+    
+}
