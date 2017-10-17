48bc21f8bcbeca86b54ae9f733bd29d29f23ceff
remid0 <remid.2002@gmail.com>
2017-10-17 18:50:59 +0200

    abstract class Message

---
+++ Battleship/src/main/java/com/utclo23/com/messages/Message.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utclo23.com.messages;
+
+import java.io.Serializable;
+import com.utclo23.data.structure.PublicUser;
+
+/**
+ *
+ * @author remid
+ */
+public abstract class Message implements Serializable{
+    PublicUser user;
+    
+    abstract void callaback();
+}
