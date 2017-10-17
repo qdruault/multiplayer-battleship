a79e23277d0dc44a16b08f1b55c38d84ebc60806
chicheth <thibault.chiche@etu.utc.fr>
2017-10-17 17:41:16 +0200

    Ajout prototypes façades

--- Battleship/src/main/java/com/utclo23/com/ComFacade.java
+++ Battleship/src/main/java/com/utclo23/com/ComFacade.java
@@ -4,11 +4,40 @@
  * and open the template in the editor.
  */
 package com.utclo23.com;
-
+import com.utclo23.data.structure.PublicUser;
+import com.utclo23.data.structure.Ship;
+import com.utclo23.data.structure.Mine;
+import com.utclo23.data.structure.StatGame;
 /**
  *
  * @author thibault
  */
 public class ComFacade {
-    
+    void sendShipsToEnnemy(Ship[] listShips, PublicUser dest){
+        
+    }
+    void notifyUserSignedIn(PublicUser user){
+
+    }
+    void notifyUserSignedOut(PublicUser user){
+
+    }
+    void notifyNewMessage(Message message){
+
+    }
+    void notifyNewCoordinates(Mine mine, PublicUser[] recipient){
+
+    }
+    void notifyNewGame(StatGame game){
+
+    }
+    void connectionToGame(StatGame game){
+
+    }
+    void leaveGame(PublicUser user){
+
+    }
+    void sendDiscovery(){
+
+    }
 }
