8a809a4d3140e4843cc86949db49914cad7f97d2
dkonam <mail.david.konam@gmail.com>
2017-10-17 17:41:31 +0200

    classes : Game + classic + belgian

--- Battleship/src/main/java/com/utclo23/data/structure/Game.java
+++ Battleship/src/main/java/com/utclo23/data/structure/Game.java
@@ -5,10 +5,38 @@
  */
 package com.utclo23.data.structure;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Davy
  */
-public class Game {
+public abstract class Game {
+    private StatGame statGame;
+    private ArrayList<Player> players;
+    private ArrayList<LightPublicUser> spectators;
+    private ArrayList<Message> messages;
+
+    public Game(StatGame statGame, ArrayList<Player> players, ArrayList<LightPublicUser> spectators, ArrayList<Message> messages) {
+        this.statGame = statGame;
+        this.players = players;
+        this.spectators = spectators;
+        this.messages = messages;
+    }
+    
+    public ArrayList<LightPublicUser> getRecipients()
+    {
+        return null;
+    }
+
+    public StatGame getStatGame() {
+        return statGame;
+    }
+
+    public void setStatGame(StatGame statGame) {
+        this.statGame = statGame;
+    }
+    
+    
     
 }
