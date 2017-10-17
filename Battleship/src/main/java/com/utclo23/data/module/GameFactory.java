d5eb6fcadb7c1fe5b2ea10449b871d6bad6fe4ff
tboulair <tboulair@utc>
2017-10-17 18:28:52 +0200

    GameFactory.

---
+++ Battleship/src/main/java/com/utclo23/data/module/GameFactory.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.utclo23.data.module;
+
+/**
+ *
+ * @author tboulair
+ */
+public class GameFactory {
+    
+}
