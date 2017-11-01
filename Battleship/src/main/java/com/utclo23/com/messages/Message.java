83474d9edea46215360069e87dcdd665ee6c71b7
chicheth <thibault.chiche@etu.utc.fr>
2017-11-01 10:41:56 +0100

    ajout de l'attribut IP_Sender à la classe Message

--- Battleship/src/main/java/com/utclo23/com/messages/Message.java
+++ Battleship/src/main/java/com/utclo23/com/messages/Message.java
@@ -7,7 +7,7 @@ package com.utclo23.com.messages;
 
 import java.io.Serializable;
 import com.utclo23.data.structure.PublicUser;
-
+import java.net.Inet4Address;
 /**
  *
  * @author remid
@@ -15,6 +15,6 @@ import com.utclo23.data.structure.PublicUser;
 public abstract class Message implements Serializable{
     PublicUser user;
     String name;
-    
+    private Inet4Address IP_sender;
     public abstract void callback();
 }
