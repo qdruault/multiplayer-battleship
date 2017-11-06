55c1c3828fca163e85eab8b96f0db43b3e90cd68
Peining <lipeining.utc@gmail.com>
2017-11-07 00:47:19 +0100

    Update controller of PlayerProfile

--- Battleship/src/main/java/com/utclo23/ihmmain/controller/PlayerProfileController.java
+++ Battleship/src/main/java/com/utclo23/ihmmain/controller/PlayerProfileController.java
@@ -10,7 +10,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 /**
  *
  * @author Linxuhao
@@ -19,12 +21,32 @@ public class PlayerProfileController extends AbstractController{
     @FXML
     private Label label;
     @FXML
+    private TextField description;
+
+    @FXML
     private Button backButton;
+    @FXML
+    private Button othersButton;
+    @FXML
+    private Button editDesc;
 
     @FXML
     private void back(ActionEvent event) throws IOException{
         ihmmain.toMenu();
     }
     
-    
+    @FXML
+    private void editDesc(ActionEvent event) throws IOException{
+        String text;
+        description.setEditable(true);
+        text = description.getText();
+        description.setText(text);
+    }
+    @FXML
+     private void closeEdit(KeyEvent event) throws IOException{
+        KeyCode code = event.getCode();
+        if (code == KeyCode.ENTER){
+            description.setEditable(false);
+        }
+    }
 }
