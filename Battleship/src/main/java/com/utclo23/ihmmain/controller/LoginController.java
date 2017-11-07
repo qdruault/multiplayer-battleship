387b38d6c59e0d9861a8b5289208aa3ff2c224b9
Camille Quenin <camille.quenin@gmail.com>
2017-11-07 11:45:41 +0100

    add javadoc

--- Battleship/src/main/java/com/utclo23/ihmmain/controller/LoginController.java
+++ Battleship/src/main/java/com/utclo23/ihmmain/controller/LoginController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 /**
+ * Controller of the login page.
+ * 
  * @author Camille Quenin
  * @author Linxuhao
  */
@@ -39,6 +41,14 @@ public class LoginController extends AbstractController{
         System.exit(0);
     }
     
+    /**
+     * Checks if the usernameField and the passwordField are not empty
+     * and changes the color of their border accordingly (red or grey).
+     * 
+     * @param username
+     * @param password
+     * @return true if they are not empty, false otherwise
+     */
     private boolean fieldsAreNotEmpty(String username, String password){
         usernameField.setStyle("");
         passwordField.setStyle("");
