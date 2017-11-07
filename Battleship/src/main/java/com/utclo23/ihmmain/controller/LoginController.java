5c53a44109c5fe37481807b0ac00e2adecba519c
Camille Quenin <camille.quenin@gmail.com>
2017-11-07 11:18:21 +0100

    add a new GUI for the login page and update the controller accordingly

--- Battleship/src/main/java/com/utclo23/ihmmain/controller/LoginController.java
+++ Battleship/src/main/java/com/utclo23/ihmmain/controller/LoginController.java
@@ -1,35 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.utclo23.ihmmain.controller;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
- *
+ * @author Camille Quenin
  * @author Linxuhao
  */
 public class LoginController extends AbstractController{
+    
     @FXML
-    private Label label;
+    private TextField usernameField;  
+    
     @FXML
-    private Button loginButton;
-    @FXML
-    private Button createUserButton;
+    private PasswordField passwordField;
 
     @FXML
-    private void login(ActionEvent event) throws IOException{
-        ihmmain.toMenu();
+    private void loginAction(ActionEvent event) throws IOException{
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        
+        if(fieldsAreNotEmpty(username, password)){
+            //TODO : add IDataIHMMain.signin(username, password) within a try/catch block
+            ihmmain.toMenu();
+        }
     }
-
+    
     @FXML
-    private void createUser(ActionEvent event) throws IOException{
+    private void createUserAction(ActionEvent event) throws IOException{
         ihmmain.toCreateUser();
+    }
+    
+    @FXML
+    private void exitAction(ActionEvent event) throws IOException{
+        System.exit(0);
+    }
+    
+    private boolean fieldsAreNotEmpty(String username, String password){
+        usernameField.setStyle("");
+        passwordField.setStyle("");
+        boolean notEmpty = true;
+        
+        if(username.length() == 0){
+            usernameField.setStyle("-fx-border-color: red;"); 
+            notEmpty = false;
+        }        
+        if(password.length() == 0){
+            passwordField.setStyle("-fx-border-color: red;");
+            notEmpty = false;
+        }
+        return notEmpty;
     }
 }
