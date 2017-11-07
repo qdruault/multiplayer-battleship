5bc322ccb0aa40a14c3fac8ffb0e8aec5073eebf
Camille Quenin <camille.quenin@gmail.com>
2017-11-07 17:09:46 +0100

    clear code

--- Battleship/src/main/java/com/utclo23/ihmmain/controller/LoginController.java
+++ Battleship/src/main/java/com/utclo23/ihmmain/controller/LoginController.java
@@ -3,8 +3,6 @@ package com.utclo23.ihmmain.controller;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
