def14dfd68b295ecfc6ddf20e4d03f7032a1f0a3
Théo Hordequin <theo.hordequin@gmail.com>
2017-11-14 19:14:55 +0100

    Add ids on button and right fxml controller

---
+++ Battleship/src/main/java/com/utclo23/ihmtable/controller/InGameGUIMenuController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utclo23.ihmtable.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author theohordequin
+ */
+public class InGameGUIMenuController {
+    
+    @FXML
+    private Button inGameGUIReview;
+
+    @FXML
+    private Button inGameGUIReturn;
+}
