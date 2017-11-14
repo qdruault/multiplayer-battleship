dd22d1096f2de7b2cfc7125848093cbedcc4389e
Peining <lipeining.utc@gmail.com>
2017-11-14 12:27:14 +0100

    Update interface PlayerProfile

--- Battleship/src/main/java/com/utclo23/ihmmain/controller/PopupController.java
+++ Battleship/src/main/java/com/utclo23/ihmmain/controller/PopupController.java
@@ -1,28 +1,30 @@
 package com.utclo23.ihmmain.controller;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.TextArea;
 
 /**
  * FXML Controller class
  *
  * @author lipeining
  */
-public class PopupController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+public class PopupController extends AbstractController{  
     
+    PlayerProfileController profile;
+    @FXML
+    private TextArea field;
+    @FXML
+    private void close(ActionEvent event) throws IOException{
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
+    @FXML
+    private void updateID(ActionEvent event) throws IOException{
+        String text;
+        text = field.getText();
+        //System.out.println(profile.testUserID);
+        ((Node) (event.getSource())).getScene().getWindow().hide();
+    }
 }
