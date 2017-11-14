84d2c50e79ccdf8f1cbcfb7f7b221a3ecc86857f
Lin xuhao <xuhao.lin@etu.utc.fr>
2017-11-14 18:25:01 +0100

    delete the system out

--- Battleship/src/main/java/com/utclo23/data/facade/DataFacade.java
+++ Battleship/src/main/java/com/utclo23/data/facade/DataFacade.java
@@ -266,7 +266,6 @@ public class DataFacade implements IDataCom, IDataIHMTable, IDataIHMMain {
     @Override
     public PublicUser getMyPublicUserProfile() {
         try {
-            System.out.println("start get userProfile from user mediator");
             return this.userMediator.getMyPublicUserProfile();
         } catch (Exception e) {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
