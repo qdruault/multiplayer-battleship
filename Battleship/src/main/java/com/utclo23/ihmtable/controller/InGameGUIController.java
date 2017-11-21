0c3ae2764f23f870a2dcb1b56bafb5891a197173
Théo Hordequin <theo.hordequin@gmail.com>
2017-11-21 17:42:47 +0100

    Fix import on controllerGUI

--- Battleship/src/main/java/com/utclo23/ihmtable/controller/InGameGUIController.java
+++ Battleship/src/main/java/com/utclo23/ihmtable/controller/InGameGUIController.java
@@ -18,6 +18,9 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -31,6 +34,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  *
