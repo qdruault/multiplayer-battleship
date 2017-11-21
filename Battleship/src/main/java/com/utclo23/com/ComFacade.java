1e60cdab4e04e03a04588f4b55f1e60d212ef119
Soukaina Didi Alaoui <air@MacBook-Air-de-AIR.local>
2017-11-21 18:59:31 +0100

    javadoc correction 3

--- Battleship/src/main/java/com/utclo23/com/ComFacade.java
+++ Battleship/src/main/java/com/utclo23/com/ComFacade.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 /**
  * Facade for the communication module
- *
  * @author Thibault CHICHE
  */
 public class ComFacade {
@@ -45,9 +44,8 @@ public class ComFacade {
     * Called to send all ships coordinates to the other player of the game, 
     * after placing all ships on the board in order to update other player's 
     * Data module. 
-    * @param listShips   List of placed ships
-    * @param dest The player who must receive ships coordinates
-    * @return nothing
+    * @param listShips is the list of placed ships
+    * @param dest is the player who must receive the ships coordinates
     */
     public void sendShipsToEnnemy(List<Ship> listShips, PublicUser dest){
         M_PlaceShip m_placeship = new M_PlaceShip(iDataCom.getMyPublicUserProfile(), listShips);
@@ -57,7 +55,6 @@ public class ComFacade {
 
     /**
     * Called to initialize attribute kIpCtrl's knownIp hashmap.
-    * @return nothing
     */
     public void notifyUserSignedIn(PublicUser user){
         kIpCtrl.initIpList(iDataCom);
@@ -71,8 +68,7 @@ public class ComFacade {
 
     /**
     * Called to send "log out" notification to everybody.
-    * @param user   current user
-    * @return nothing
+    * @param user is the current user
     */
     public void notifyUserSignedOut(PublicUser user){
         M_Deconnection m_deconnexion = new M_Deconnection(user);
@@ -84,8 +80,7 @@ public class ComFacade {
 
     /**
     * Called to send a chat message to all the recipients of the message.
-    * @param message   Contains message and recipients
-    * @return nothing
+    * @param message is the message content and recipients
     */
     public void notifyNewMessage(com.utclo23.data.structure.Message message) {
         M_Chat m_chat = new M_Chat(iDataCom.getMyPublicUserProfile(), message, message.getTimestamp());
@@ -97,9 +92,8 @@ public class ComFacade {
 
     /**
     * Called to send new mine to the other player and the spectators. 
-    * @param mine   New placed mine
-    * @param recipients recipients of the new mine
-    * @return nothing
+    * @param mine is the new placed mine
+    * @param recipients are the recipients of the new mine
     */
     public void notifyNewCoordinates(Mine mine, List<LightPublicUser> recipients){
         M_PlaceMine m_placemine = new M_PlaceMine(iDataCom.getMyPublicUserProfile(), mine);
@@ -111,9 +105,8 @@ public class ComFacade {
 
     /**
     * Called to notify everybody of the creation of a new game to update all 
-    * Data's module. 
-    * @param game   New created game
-    * @return nothing
+    * users Data's module. 
+    * @param game is the new created game
     */
     public void notifyNewGame(StatGame game){
         M_CreationGame m_creationgame = new M_CreationGame(iDataCom.getMyPublicUserProfile(), game);
@@ -124,9 +117,8 @@ public class ComFacade {
     }
 
     /**
-    * Called to send a demand of joining the specified game.
-    * @param game   Game you want to join
-    * @return nothing
+    * Called to send a request to join a specified game.
+    * @param game is a game the user wants to join
     */
     public void connectionToGame(StatGame game){
         M_JoinGame m_joingame = new M_JoinGame(iDataCom.getMyPublicUserProfile(), game);
@@ -137,8 +129,7 @@ public class ComFacade {
 
     /**
     * Called to send "leave game" notification to everybody.
-    * @param user   List of placed ships
-    * @return nothing
+    * @param user is the user who wants to leave the game
     */
     public void leaveGame(PublicUser user){
         M_LeaveGame m_leavegame = new M_LeaveGame(iDataCom.getMyPublicUserProfile());
@@ -150,9 +141,8 @@ public class ComFacade {
 
     /**
     * Called to launch network discovery.
-    * @param user   List of placed ships
-    * @param listIpTarget List of ip targets
-    * @return nothing
+    * @param user is the user who starts the sendDiscovery
+    * @param listIpTarget is the list of known ip targets
     */
     public void sendDiscovery(PublicUser user, List<Inet4Address> listIpTarget) {
         
@@ -166,10 +156,9 @@ public class ComFacade {
     }
 
     /**
-    * Called to send a demand of getting to the specified recipient's 
-    * PublicUserInfo.
-    * @param id   UID of the recipient
-    * @return nothing
+    * Called to send a request to get detailed information of a specified 
+    * user.
+    * @param id is the UID of the recipient
     */
     public void getPublicUserProfile(String id){
         M_GetPlayerInfo m_getplayerinfo = new M_GetPlayerInfo(iDataCom.getMyPublicUserProfile());
@@ -178,13 +167,12 @@ public class ComFacade {
     }
 
     /**
-    * If success, called to send a join game notification to everybody with the 
-    * player who joins the game.Otherwise, the player who wanted to join the 
-    * game is advertise that it fails.
-    * @param success True if the play can join the game, false otherwise 
-    * @param id UID of the player demanding to join the game
-    * @param game The game in question
-    * @return nothing
+    * If success, send a 'join game' notification to everybody with the 
+    * player who joined the game. Otherwise, the player who wanted to join the 
+    * game is advertised that it failed.
+    * @param success is true if the player can join the game, false otherwise 
+    * @param id is the UID of the player demanding to join the game
+    * @param game is the game in question
     */
     public void joinGameResponse (boolean success, String id, StatGame game){
         M_JoinGameResponse m_joingameresponse = new M_JoinGameResponse(iDataCom.getMyPublicUserProfile(), success);
