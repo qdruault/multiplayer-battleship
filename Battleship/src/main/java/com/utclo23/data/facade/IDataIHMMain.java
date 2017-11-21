9680e30492513980b548a4334d9400c7383c44cd
Audrick <audrick.libraire@etu.utc.fr>
2017-11-21 18:25:16 +0100

    [ihm-main] add createGameController and GUI

--- Battleship/src/main/java/com/utclo23/data/facade/IDataIHMMain.java
+++ Battleship/src/main/java/com/utclo23/data/facade/IDataIHMMain.java
@@ -29,7 +29,6 @@ public interface IDataIHMMain {
     public void signOut() throws Exception;
     public List<LightPublicUser> getConnectedUsers();
     public PublicUser getMyPublicUserProfile();
-
     public List<String> getIPDiscovery();
     public void setIPDiscovery(List<String> discoveryNodes) throws DataException;
 
