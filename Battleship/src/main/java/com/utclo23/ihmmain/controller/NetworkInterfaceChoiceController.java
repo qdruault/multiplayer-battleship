80c1f21225a9b57c039fb010747c4902c1b3c1a8
Louis Groisne <louis.groisne@gmail.com>
2017-12-19 18:20:09 +0100

    add javadoc for the NetworkInterfaceChoiceController

--- Battleship/src/main/java/com/utclo23/ihmmain/controller/NetworkInterfaceChoiceController.java
+++ Battleship/src/main/java/com/utclo23/ihmmain/controller/NetworkInterfaceChoiceController.java
@@ -24,17 +24,27 @@ public class NetworkInterfaceChoiceController extends AbstractController {
     @FXML
     ListView listNetworks;
     
+    
+    /**
+     * Handler for the exit button.
+     * Quit the application.
+     * @param event 
+     */
     @FXML
     private void exit(ActionEvent event){
         System.exit(0);
     }
-        
+    
     @Override
     public void start(){
         lookForInterface();
         listNetworks.getSelectionModel().select(0);
     }
     
+    /**
+     * Look for all the available network interfaces on the device.
+     * Add all valid ipv4 interfaces to the listNetworks list.
+     */
     private void lookForInterface(){
         try {
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
@@ -48,7 +58,12 @@ public class NetworkInterfaceChoiceController extends AbstractController {
             throw new RuntimeException(e);
         }
     }
-    
+    /**
+     * Get the chosen network interface.
+     * Get the associated network adress and set Data with it.
+     * @param event
+     * @throws Exception 
+     */
     @FXML 
     private void handleButtonValidate(ActionEvent event) throws Exception{
         Inet4Address usedIf;
@@ -77,6 +92,13 @@ public class NetworkInterfaceChoiceController extends AbstractController {
         }   
     }
     
+    /**
+     * Check the given network interface.
+     * Iterate through all the interface addresses available on the network 
+     * interface to look for an ipv4 adress.
+     * @param networkInt: networkInterface to check.
+     * @return true if networkInt has a valid ipv4 address available. 
+     */
     private boolean isValid(NetworkInterface networkInt){
         Inet4Address usedIf;
         Boolean isValid = false;
